3ff68e9b40757b9c1fd81313ea2633596721a90d
Eric Lee <me@ericchanlee.com>
2026-06-13 00:57:06 -0700

    feat: add nix flake to build

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "GLML Documentation";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    glml.url = "github:glml-lang/glml";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      glml,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        glml-bin = glml.packages.${system}.default;
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            mdbook
+            nodejs
+            python3
+            glml-bin
+          ];
+        };
+      }
+    );
+}
